0671bf8f0662c80cdf6345b44980d63707d4e0d0
nisbus <nisbus@gmail.com>
2011-07-13 23:53:15 +0000

    fixed the recent-items view to output new data as added.

--- babelstat_couchapps/babelstat_gui/views/recent-items/map.erl
+++ babelstat_couchapps/babelstat_gui/views/recent-items/map.erl
@@ -1,8 +1,9 @@
 fun({Doc}) ->
-	case proplists:get_value(<<created_at>>,Doc,null) of
-		null ->
-	            undefined;
-		CreatedAt ->
-		    Emit(CreatedAt, {Doc})
+	case proplists:get_value(<<"type">>, Doc, null) of
+	    null ->
+		undefined;
+	    _ ->
+		Created = proplists:get_value(<<"created_date">>, Doc),
+		Emit(Created, {Doc})
 	end
 end.
